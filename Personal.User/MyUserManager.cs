2971bef621f918d39befcfb5f6d829b933769c02
Azat Buriev <azat@hightech.today>
2016-02-01 16:50:44 +0300

    fix

---
+++ Personal.User/MyUserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Personal.Domain.Entities;
+
+namespace Personal.User
+{
+    public class MyUserManager : UserManager<Customer, int>
+    {
+        public MyUserManager(IUserStore<Customer, int> store) : base(store)
+        {
+            UserTokenProvider = new TotpSecurityStampBasedTokenProvider<Customer, int>();
+            UserValidator = new UserValidator<Customer, int>(this)
+            {
+                AllowOnlyAlphanumericUserNames = false
+            };
+        }
+
+        public static MyUserManager Get(DbContext dncontext)
+        {
+            return new MyUserManager(new MyUserStore(dncontext));
+        }
+    }
+}
---
+++ Personal.User/MyUserStore.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Personal.Domain.Entities;
+
+namespace Personal.User
+{
+    public class MyUserStore : UserStore<Customer, IdentityRole<int, IdentityUserRole<int>>, int, IdentityUserLogin<int>, IdentityUserRole<int>, IdentityUserClaim<int>>
+    {
+        public MyUserStore(DbContext ctx) : base(ctx) { }
+
+        /// <summary>
+        /// Insert an entity
+        /// </summary>
+        /// <param name="user"/>
+        public override Task CreateAsync(Customer user)
+        {
+            Context.Set<Customer>().Add(user);
+
+            Context.Entry(user).State = EntityState.Added;
+            return Context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Update an entity
+        /// </summary>
+        /// <param name="user"/>
+        public override Task UpdateAsync(Customer user)
+        {
+            return Context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Mark an entity for deletion
+        /// </summary>
+        /// <param name="user"/>
+        public override Task DeleteAsync(Customer user)
+        {
+            return UpdateAsync(user);
+        }
+        /// <summary>
+        /// Find a user by id
+        /// </summary>
+        /// <param name="userId"/>
+        /// <returns/>
+        public override async Task<Customer> FindByIdAsync(int userId)
+        {
+            return await Context.Set<Customer>().FirstOrDefaultAsync(u => u.Id == userId);
+        }
+        /// <summary>
+        /// Find a user by name
+        /// </summary>
+        /// <param name="userName"/>
+        /// <returns/>
+        public override async Task<Customer> FindByNameAsync(string userName)
+        {
+            return await Context.Set<Customer>().FirstOrDefaultAsync(u => Equals(u.UserName, userName));
+        }
+    }
+}
