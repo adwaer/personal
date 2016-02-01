97310d47f7440dc281c9ad8ecc09acd1ea9ff91b
Azat Buriev <azat@hightech.today>
2016-02-01 17:03:05 +0300

    gulp

---
+++ Personal.Web/bundle.config.js
@@ -0,0 +1,67 @@
+﻿// bundle.config.js
+
+var lazypipe = require("lazypipe");
+var babel = require("gulp-babel");
+
+var compatTransforms = lazypipe()
+    .pipe(babel, {
+        presets: ["es2015"]
+    });
+
+module.exports = {
+    bundle: {
+        "vendor": {
+            scripts: [
+                "bower_components/jquery/dist/jquery.min.js",
+                "bower_components/angular/angular.min.js",
+                "bower_components/angular-route/angular-route.min.js",
+                "bower_components/angular-resource/angular-resource.min.js",
+                "bower_components/angular-smart-table/dist/smart-table.min.js",
+                "bower_components/underscore/underscore-min.js",
+                "bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js",
+                "bower_components/moment/min/moment.min.js",
+                "bower_components/moment/locale/ru.js",
+                "bower_components/angular-ui-calendar/src/calendar.js",
+                "bower_components/fullcalendar/dist/fullcalendar.min.js",
+                "bower_components/fullcalendar/dist/locale/ru.js"
+            ],
+            styles: [
+                "bower_components/bootstrap/dist/css/bootstrap.min.css",
+                "bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
+                "bower_components/fullcalendar/dist/fullcalendar.css"
+            ],
+            options: {
+                rev: false
+            }
+        },
+        "main": {
+            scripts: "Frontend/**/*.js",
+            styles: [
+                "Content/Site.css",
+                "Content/calendarDemo.css"
+            ],
+            options: {
+                transforms: {
+                    scripts: compatTransforms
+                },
+                pluginOptions: {
+                    'gulp-uglify': {
+                        mangle: false
+                    }
+                },
+                rev: false
+            }
+        }
+        //,
+        //"load-gulp-bundles": {
+        //    scripts: 'Scripts/load-gulp-bundles.js',
+        //    options: {
+        //        transforms: {
+        //            scripts: compatTransforms
+        //        },
+        //        rev: false
+        //    }
+        //}
+    }
+
+};
---
+++ Personal.Web/gulpfile.js
@@ -0,0 +1,85 @@
+﻿// gulpfile.js 
+"use strict";
+
+var gulp = require("gulp");
+var bundle = require("gulp-bundle-assets");
+var del = require("del");
+var notify = require("gulp-notify");
+var path = require("path");
+var directoryMap = require("gulp-directory-map");
+var browserSync = require("browser-sync").create();
+
+// settings
+var destDir = "./Content/dist";
+var pathPrefix = "/Content/dist/";
+var statTemplatesDir = "Frontend/Stat/templates";
+var siteUrl = "http://localhost:9165/";
+
+gulp.task("bundle", ["bundle-clean"], function () {
+    return gulp.src("./bundle.config.js")
+        .pipe(bundle())
+        .pipe(bundle.results({
+            dest: destDir,
+            pathPrefix: pathPrefix
+        }))
+        .pipe(gulp.dest(destDir));
+});
+
+gulp.task("bundle-clean", function () {
+    gulp.src(".").pipe(notify("bundling started"));
+    clean();
+});
+
+gulp.task("clean", function () {
+    clean();
+});
+
+gulp.task("watch-only", function () {
+    watchBundles();
+});
+
+gulp.task("watch", function () {
+    browserSync.init({
+        proxy: siteUrl
+    });
+
+    watchBundles();
+});
+
+gulp.task("reload-and-notify", function () {
+    browserSync.reload();
+    gulp.src(".").pipe(notify("bundling finished"));
+});
+
+gulp.task("stat-templates", function () {
+    gulp.src(statTemplatesDir + "/**/*.html")
+      .pipe(directoryMap({
+          filename: "stat-templates.json"
+          //prefix: statTemplatesDir
+      }))
+      .pipe(gulp.dest(destDir));
+});
+
+gulp.task("default", ["bundle", "stat-templates"], function () {
+    //gulp.start("reload-and-notify");
+});
+
+function clean() {
+    del(destDir + "/**/*.*");
+    gulp.src(".").pipe(notify("cleanup finished"));
+}
+
+function watchBundles() {
+    gulp.watch([destDir + "/*.*", "Views/**/*.cshtml", "Frontend/**/*.html"], ["reload-and-notify"]);
+
+    var destDirAbsolutePath = path.join(__dirname, destDir);
+
+    bundle.watch({
+        configPath: path.join(__dirname, "bundle.config.js"),
+        results: {
+            dest: destDirAbsolutePath,
+            pathPrefix: pathPrefix
+        },
+        dest: destDirAbsolutePath
+    });
+}
