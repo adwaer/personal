a9c4f4a3850f92b745a246413579edcbde39afb2
Azat <adwaer@live.ru>
2016-04-15 02:45:29 +0300

    web api methods

---
+++ MQ.IpLoc/MQ.WebApi/Controllers/LocationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MQ.WebApi.Controllers
+{
+    public class LocationController : ApiController
+    {
+        [HttpGet]
+        [Route("ip/location/{ip}")]
+        public async Task<IHttpActionResult> LocationByIp(string ip)
+        {
+            return Ok(ip);
+        }
+
+        [HttpGet]
+        [Route("city/locations/{city}")]
+        public async Task<IHttpActionResult> LocationByCity(string city)
+        {
+            return Ok(city);
+        }
+    }
+}
